2080bd6635de10d40d6621fab187382b322da5f4
Robin Kadergran <titi_malaki@live.se>
2026-05-17 16:02:25 +0200

    fixed from dic to set

--- classifier/views.py
+++ classifier/views.py
@@ -10,11 +10,11 @@ model = ImageClassifier()
 @csrf_exempt
 def predict(request):
     if request.method != "POST":
-        return JsonResponse({"Error: Only POST Request allowed."}, status=405)
+        return JsonResponse({"Error": "Only POST Request allowed."}, status=405)
     
     
     if "image" not in request.FILES:
-        return JsonResponse({"Error: No Image provided."}, status=400)
+        return JsonResponse({"Error": "No Image provided."}, status=400)
     
     img_file = request.FILES["image"]
     label, confidence, all_probs = model.predict_bytes(img_file.read())
